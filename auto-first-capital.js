24b26c04fa7cae93a4baa70bd0cb55968dffa20e
Syarol <1oleq25@gmail.com>
2018-08-22 13:54:15 +0300

    Some updates
    - Now letter elevate after "!" and "?" symbols
    - If write among the text, elevating also working
    - Fixed bug with caret position inside "div" elements

--- auto-first-capital.js
+++ auto-first-capital.js
@@ -10,51 +10,68 @@ function getFocused(){
 	return document.activeElement;
 }
 
-document.onclick = () => {
+function getElementIndex(node) {
+	let index = 0;
+	while ( (node = node.previousElementSibling) ){
+		index++;
+	}
+	return index;
+}
+
+window.onclick = () => {
 	let focused = getFocused();
 
 	focused.onkeypress = function(e){
 		if (e.code === 'Space'){
 			if (this.tagName == 'DIV'){
-				let caretPosInit = window.getSelection().anchorOffset;
-				let caretPos = caretPosInit;
+				let sel = window.getSelection();
+				let nodeIndex = getElementIndex(sel.anchorNode);
+				let caretPosInit = sel.anchorOffset;
+				let caretPos = caretPosInit - 1;
 
-				while (caretPos >= 0){
-					if (this.textContent[caretPos] == ' ' && this.textContent[caretPos - 1] == '.'){
+				while (caretPos >= -1){
+					if (this.textContent[caretPos] == ' ' && this.textContent[caretPos - 1] == '.' || 
+						this.textContent[caretPos] == ' ' && this.textContent[caretPos - 1] == '?' || 
+						this.textContent[caretPos] == ' ' && this.textContent[caretPos - 1] == '!'){
 						this.textContent = [this.textContent.substring(0, caretPos+1), this.textContent.charAt(caretPos+1).toUpperCase(), this.textContent.slice(caretPos+2)].join('');	
 						break;
 					}else if (this.textContent[caretPos] == undefined) {
 						this.textContent = this.textContent.charAt(0).toUpperCase() + this.textContent.slice(1);
-					}else if (this.textContent[caretPos] == '.') {
+						break;
+					}else if (this.textContent[caretPos] == '.' || this.textContent[caretPos] == '?' || this.textContent[caretPos] == '!') {
 						this.textContent = [this.textContent.substring(0, caretPos+1), this.textContent.charAt(caretPos+1).toUpperCase(), this.textContent.slice(caretPos+2)].join('');	
 						break;
-					} 
-					caretPos--;
+					}else caretPos--;
 				}
 
-				var range = document.createRange();
-				var sel = window.getSelection();
-				range.setStart(this.firstChild, caretPosInit);
+				let range = document.createRange();
+				range.setStart(this.childNodes[nodeIndex], caretPosInit);
 				range.collapse(true);
 				sel.removeAllRanges();
 				sel.addRange(range);
-			
-			} else if (this.tagName == 'INPUT' || this.tagName == 'TEXTAREA'){
-				let caretPosInit = e.target.value.length;
-				let caretPos = caretPosInit;
 
-				while (caretPos >= 0){
-					if (this.value[caretPos] == ' ' && this.value[caretPos - 1] == '.'){
+			} else if (this.tagName == 'INPUT' || this.tagName == 'TEXTAREA'){
+				console.log(this.selectionStart);
+				let caretPosInit = this.selectionStart;
+				let caretPos = caretPosInit - 1;
+
+				while (caretPos >= -1){
+					if (this.value[caretPos] == ' ' && this.value[caretPos - 1] == '.' ||
+						this.value[caretPos] == ' ' && this.value[caretPos - 1] == '?' ||
+						this.value[caretPos] == ' ' && this.value[caretPos - 1] == '!'){
 						this.value = [this.value.substring(0, caretPos+1), this.value.charAt(caretPos+1).toUpperCase(), this.value.slice(caretPos+2)].join('');	
 						break;
 					}else if (this.value[caretPos] == undefined) {
 						this.value = this.value.charAt(0).toUpperCase() + this.value.slice(1);
-					}else if (this.value[caretPos] == '.') {
+						break;
+					}else if (this.value[caretPos] == '.' || this.value[caretPos] == '?' || this.value[caretPos] == '!') {
 						this.value = [this.value.substring(0, caretPos+1), this.value.charAt(caretPos+1).toUpperCase(), this.value.slice(caretPos+2)].join('');	
 						break;
-					} 
-					caretPos--;
+					}else caretPos--;
 				}
+
+				this.focus();
+                this.setSelectionRange(caretPosInit, caretPosInit);
 			}
 		}
 	};
