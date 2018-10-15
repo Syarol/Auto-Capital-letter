c5706d92670a9466e3e0c73d68d5812607f257b3
Syarol <1oleq25@gmail.com>
2018-10-15 16:33:05 +0300

    Improved code readability
    - Added comments to code
    - Code optimization

--- auto-first-capital.js
+++ auto-first-capital.js
@@ -16,11 +16,13 @@ var keyLogs = [];
  * Functions
 */
 
+/*returns element that has focus*/
 function getFocused(){
 	console.log(document.activeElement);
 	return document.activeElement;
 }
 
+/*returns sequence number of element inside node*/
 function getElementIndex(node) {
 	let index = 0;
 	while ( (node = node.previousElementSibling) ){
@@ -29,188 +31,170 @@ function getElementIndex(node) {
 	return index;
 }
 
+/*elevates the first letter inside a sentence*/
 function capitalUp(textField){
-	let spaceNum = 0;
-
 	if (textField.tagName == 'DIV'){
 		let sel = window.getSelection();
-		let nodeIndex = getElementIndex(sel.anchorNode);
-		let caretPosInit = sel.anchorOffset;
+		let nodeIndex = getElementIndex(sel.anchorNode);//get text node position inside contenteditable div
+		let caretPosInit = sel.anchorOffset;//get caret position
 		let caretPos = caretPosInit - 1;
+		let text = textField.textContent;
 
+		/*checks terms of letter elevating. If situations respond to conditions then elevates letter*/
 		while (caretPos >= -1){
-			if (textField.textContent[caretPos] == ' ') spaceNum++;
-
-			if (spaceNum == 0){
-				if (textField.textContent[caretPos] == ' ' && textField.textContent[caretPos - 1] == ' ' ||
-					textField.textContent[caretPos] == ' ' && textField.textContent[caretPos - 1] === undefined ||
-					textField.textContent[caretPosInit - 1] === undefined ||
-					[',', ';', ':', '\'', '"', '(', ')' ].includes(textField.textContent[caretPos]) ||
-					['.', '?', '!'].includes(textField.textContent[caretPosInit - 1])	){
-					upHappens = false; 
-					break;
-				}else if (textField.textContent[caretPos] == ' ' && textField.textContent[caretPos - 1] == '.' || 
-					textField.textContent[caretPos] == ' ' && textField.textContent[caretPos - 1] == '?' || 
-					textField.textContent[caretPos] == ' ' && textField.textContent[caretPos - 1] == '!' ||
-					['.', '?', '!'].includes(textField.textContent[caretPos])){
-					textField.textContent = textField.textContent.substring(0, caretPos+1) + textField.textContent.charAt(caretPos+1).toUpperCase() + textField.textContent.slice(caretPos+2);	
+			if (text[caretPos] == ' ' && [' ', undefined].includes(text[caretPos - 1]) ||
+				[',', ';', ':', '\'', '"', '(', ')' ].includes(text[caretPos]) ||
+				['.', '?', '!', undefined].includes(text[caretPosInit - 1])){
+				upHappens = false; 
+				break;
+			}else if (text[caretPos] == ' ' && ['.', '?', '!'].includes(text[caretPos - 1]) ||
+				['.', '?', '!'].includes(text[caretPos])){
+				if (text.charAt(caretPos+1) === text.charAt(caretPos+1).toUpperCase()) {
+					upHappens = false;
+				} else {
+					textField.textContent = text.substring(0, caretPos+1) + text.charAt(caretPos+1).toUpperCase() + text.slice(caretPos+2);	
 					upHappens = true; 
-					break;
-				}else if (textField.textContent[caretPos] === undefined) {
-					textField.textContent = textField.textContent.charAt(0).toUpperCase() + textField.textContent.slice(1);
+				}
+				break;
+			}else if (text[caretPos] === undefined) {
+				if (text.charAt(0) === text.charAt(0).toUpperCase()) {
+					upHappens = false;
+				} else {
+					textField.textContent = text.charAt(0).toUpperCase() + text.slice(1);
 					upHappens = true; 
-					break;
-				}else caretPos--;
-			} else break;
+				}
+				break;
+			}else caretPos--;
 		}
 
+		/*If letter was upped then returns caret position to original*/
 		if (upHappens){
-			let range = document.createRange();
+	        let range = document.createRange();
 			range.setStart(textField.childNodes[nodeIndex], caretPosInit);
 			range.collapse(true);
 			sel.removeAllRanges();
 			sel.addRange(range);
-			textField.focus();
-  			range.detach(); 
 		}
 
 	} else if (textField.tagName == 'INPUT' || textField.tagName == 'TEXTAREA'){
-		let caretPosInit = textField.selectionStart;
+		let caretPosInit = textField.selectionStart; //get caret position
 		let caretPos = caretPosInit - 1;
+		let text = textField.value;
 
+		/*checks terms of letter elevating. If situations respond to conditions then elevates letter*/
 		while (caretPos >= -1){
-			if (textField.value[caretPos] == ' ') spaceNum++;
-
-			if (spaceNum == 0){
-				if (textField.value[caretPos] == ' ' && textField.value[caretPos - 1] == ' ' ||
-					textField.value[caretPos] == ' ' && textField.value[caretPos - 1] === undefined ||
-					textField.value[caretPosInit - 1] === undefined ||
-					[',', ';', ':', '\'', '"', '(', ')' ].includes(textField.value[caretPos]) ||
-					['.', '?', '!'].includes(textField.value[caretPosInit - 1])	){
-					upHappens = false; 
-					break;
-				}else if (textField.value[caretPos] == ' ' && textField.value[caretPos - 1] == '.' ||
-					textField.value[caretPos] == ' ' && textField.value[caretPos - 1] == '?' ||
-					textField.value[caretPos] == ' ' && textField.value[caretPos - 1] == '!' ||
-					['.', '?', '!'].includes(textField.value[caretPos])){
-					textField.value = textField.value.substring(0, caretPos+1) + textField.value.charAt(caretPos+1).toUpperCase() + textField.value.slice(caretPos+2);	
+			if (text[caretPos] == ' ' && [' ', undefined].includes(text[caretPos - 1]) ||
+				[',', ';', ':', '\'', '"', '(', ')' ].includes(text[caretPos]) ||
+				['.', '?', '!', undefined].includes(text[caretPosInit - 1])	){
+				upHappens = false; 
+				break;
+			}else if (text[caretPos] == ' ' && ['.', '?', '!'].includes(text[caretPos - 1]) ||
+				['.', '?', '!'].includes(text[caretPos])){
+				if (text.charAt(caretPos+1) === text.charAt(caretPos+1).toUpperCase()) {
+					upHappens = false;
+				} else {
+					textField.value = text.substring(0, caretPos+1) + text.charAt(caretPos+1).toUpperCase() + text.slice(caretPos+2);	
 					upHappens = true; 
-					break;
-				}else if (textField.value[caretPos] === undefined) {
-					textField.value = textField.value.charAt(0).toUpperCase() + textField.value.slice(1);
+				}
+				break;
+			}else if (text[caretPos] === undefined) {
+				if (text.charAt(0) === textField.value.charAt(0).toUpperCase()) {
+					upHappens = false;
+				} else {
+					textField.value = text.charAt(0).toUpperCase() + text.slice(1);
 					upHappens = true; 
-					break;
-				}else caretPos--;
-			} else break;
+				}
+				break;
+			}else caretPos--;
 		}
 
+		/*If letter was upped then returns caret position to original*/
 		if (upHappens){
-			textField.focus();
-	        textField.setSelectionRange(caretPosInit, caretPosInit);
+			textField.focus();//return focus on field
+	        textField.setSelectionRange(caretPosInit, caretPosInit);//sets cursor position
 	    }
 	}
 }
 
-/*function cancelUp(e){
-	if(e.target.tagName == 'DIV'){
-		e.preventDefault();
-
+/*cancels previous elevating of the first letter*/
+function cancelUp(textField){
+	console.log('cancels');
+		
+	if(textField.tagName == 'DIV'){
 		let sel = window.getSelection();
-		let nodeIndex = getElementIndex(sel.anchorNode);
-		let caretPosInit = sel.anchorOffset;
+		let nodeIndex = getElementIndex(sel.anchorNode);//get text node position inside contenteditable div
+		let caretPosInit = sel.anchorOffset;//get caret position
 		let caretPos = caretPosInit - 1;
-		let fieldContent = e.target.textContent;
+		let fieldContent = textField.textContent;
 
+		/*checks terms of canceling letter elevating. If situations respond to conditions then returns an initial letter*/
 		while (caretPos >= -1){
-			if (fieldContent[caretPos] == ' ' && fieldContent[caretPos - 1] == '.' || 
-				fieldContent[caretPos] == ' ' && fieldContent[caretPos - 1] == '?' || 
-				fieldContent[caretPos] == ' ' && fieldContent[caretPos - 1] == '!' ||
+			if (fieldContent[caretPos] == ' ' && ['.', '?', '!'].includes(fieldContent[caretPos - 1]) || 
 				['.', '?', '!'].includes(fieldContent[caretPos])){
-				e.target.textContent = fieldContent.substring(0, caretPos+1) + fieldContent.charAt(caretPos+1).toLowerCase() + fieldContent.slice(caretPos+2);	
+				textField.textContent = fieldContent.substring(0, caretPos+1) + fieldContent.charAt(caretPos+1).toLowerCase() + fieldContent.slice(caretPos+2);	
 				break;
 			} else if (fieldContent[caretPos] === undefined) {
-				e.target.textContent = fieldContent.charAt(0).toLowerCase() + fieldContent.slice(1);
+				textField.textContent = fieldContent.charAt(0).toLowerCase() + fieldContent.slice(1);
 				break;
 			} else caretPos--;
 		}
-
-console.log(e.target.childNodes);
-console.log(e.target.childNodes[nodeIndex]);
-console.log(nodeIndex);
-console.log(e.target.childNodes.length);
-console.log(caretPosInit);
 
 		let range = document.createRange();
-		range.setStart(e.target.childNodes[nodeIndex], caretPosInit);
-		range.setEnd(e.target.childNodes[nodeIndex], caretPosInit);
+		range.setStart(textField.childNodes[nodeIndex], caretPosInit);
 		range.collapse(true);
 		sel.removeAllRanges();
-		e.target.focus();
 		sel.addRange(range);
-  		range.detach(); 
-console.log(sel.anchorOffset);
-	} else if(e.target.tagName == 'INPUT' || e.target.tagName == 'TEXTAREA'){
-		e.preventDefault();
-		let caretPosInit = e.target.selectionStart;
+	} else if(textField.tagName == 'INPUT' || textField.tagName == 'TEXTAREA'){
+		let caretPosInit = textField.selectionStart;//get caret position
 		let caretPos = caretPosInit - 1;
-		let fieldContent = e.target.value;
+		let fieldContent = textField.value;
 
+		/*checks terms of canceling letter elevating. If situations respond to conditions then returns an initial letter*/
 		while (caretPos >= -1){
-			if (fieldContent[caretPos] == ' ' && fieldContent[caretPos - 1] == '.' || 
-				fieldContent[caretPos] == ' ' && fieldContent[caretPos - 1] == '?' || 
-				fieldContent[caretPos] == ' ' && fieldContent[caretPos - 1] == '!' ||
+			if (fieldContent[caretPos] == ' ' && ['.', '?', '!'].includes(fieldContent[caretPos - 1]) || 
 				['.', '?', '!'].includes(fieldContent[caretPos])){
-				e.target.value = fieldContent.substring(0, caretPos+1) + fieldContent.charAt(caretPos+1).toLowerCase() + fieldContent.slice(caretPos+2);	
+				textField.value = fieldContent.substring(0, caretPos+1) + fieldContent.charAt(caretPos+1).toLowerCase() + fieldContent.slice(caretPos+2);	
 				break;
 			} else if (fieldContent[caretPos] === undefined){
-				e.target.value = fieldContent.charAt(0).toLowerCase() + fieldContent.slice(1);
+				textField.value = fieldContent.charAt(0).toLowerCase() + fieldContent.slice(1);
 				break;
 			} else caretPos--;
 		}
 
-		e.target.focus();
-        e.target.setSelectionRange(caretPosInit, caretPosInit);
+		textField.focus();//return focus on field
+        textField.setSelectionRange(caretPosInit, caretPosInit);//sets cursor position
 	}	
 
-	upHappens = false;	
+	upHappens = false;//for preventing reusing
 	keyLogs = [];
 }
-*/
+
 /**
  * Event Listeners
 */
 
-window.onkeydown = (e) => {
-	//console.log(e.keyCode);
-/*	if (upHappens && e.keyCode == 8){
-		e.preventDefault();
-		cancelUp(e);
-	}*/
-}
-
 window.onkeypress = (e) => {
 	if (keyLogs.length >= 5) keyLogs.shift();
-	keyLogs.push(e.code);
-
-	if (upHappens && e.keyCode == 8){
-		cancelUp(e);
-	} else if (upHappens && keyLogs[keyLogs.length - 1] == 'Space') {
-
-	} else if (upHappens) {
-		upHappens = false;
-		keyLogs = [];
-	}
+	keyLogs.push(e.keyCode);
 };
 
-window.onclick = () => {
+window.onmouseup = () => {
 	if (upHappens) {
 		upHappens = false;
 		keyLogs = [];
 	}
 
-	getFocused().onkeypress = (e) => {
-		if (e.code === 'Space'){
-			capitalUp(e.target);	
+	getFocused().onkeydown = (e) => {
+		if (e.keyCode === 32){
+			capitalUp(e.target);//elevates first letter in sentence	
+		} else if (upHappens && e.keyCode == 8){
+			e.preventDefault();
+			cancelUp(e.target);//cancels last letter elevating
+		} else if (upHappens && keyLogs[keyLogs.length - 1] == 32) {
+
+		} else if (upHappens) {
+			upHappens = false;
+			keyLogs = [];
 		}
 	};
 };
